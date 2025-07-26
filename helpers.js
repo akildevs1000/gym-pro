34103116167f28629d32e507cb0bc4beb95ca2ff
akildevs1000 <akildevs1000@gmail.com>
2025-07-26 20:21:27 +0400

    Initial commit

---
+++ helpers.js
@@ -0,0 +1,250 @@
+const simpleGit = require('simple-git');
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
+const os = require("os");
+const { app, Notification } = require('electron');
+
+const isDev = !app.isPackaged;
+
+let appDir;
+if (isDev) {
+    appDir = path.join(__dirname);
+} else {
+    appDir = process.resourcesPath; // where extraResources are placed
+}
+
+const networkInterfaces = os.networkInterfaces();
+
+let ipv4Address = "localhost";
+
+Object.keys(networkInterfaces).forEach((interfaceName) => {
+    networkInterfaces[interfaceName].forEach((networkInterface) => {
+        // Only consider IPv4 addresses, ignore internal and loopback addresses
+        if (networkInterface.family === "IPv4" && !networkInterface.internal) {
+            ipv4Address = networkInterface.address;
+        }
+    });
+});
+
+function tailLogFile(logFilePath) {
+    const tail = spawn('powershell.exe', [
+        '-Command',
+        `Get-Content -Path "${logFilePath}" -Wait -Tail 10`
+    ]);
+
+    tail.stdout.on('data', (data) => {
+        log(`[NGINX] ${data.toString()}`);
+    });
+
+    tail.stderr.on('data', (data) => {
+        log(`[NGINX-ERROR] ${data.toString()}`);
+    });
+}
+
+// Flexible spawn wrapper
+function spawnWrapper(mainWindow, processType, command, argsOrOptions, maybeOptions) {
+    let args = [];
+    let options = {};
+
+    if (Array.isArray(argsOrOptions)) {
+        args = argsOrOptions;
+        options = maybeOptions || {};
+    } else {
+        options = argsOrOptions || {};
+    }
+
+    const child = spawn(command, args, options);
+
+    child.stdout.on('data', (data) => {
+        log(mainWindow, `${processType} ${data.toString()}`);
+    });
+
+    child.stderr.on('data', (data) => {
+        log(mainWindow, `${processType} ${data.toString()}`);
+    });
+
+    child.on('close', (code) => {
+        log(mainWindow, `${processType} exited with code ${code} for ${JSON.stringify(argsOrOptions)}`);
+    });
+
+    child.on('error', (err) => {
+        log(mainWindow, `${processType} ${err.message}`);
+    });
+
+    return child;
+}
+
+function log(mainWindow, message) {
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    const fullMessage = `[${timestamp}] ${message}\n`;
+
+    // Send to frontend
+    if (mainWindow && mainWindow.webContents) {
+        mainWindow.webContents.send('log', fullMessage);
+    }
+
+    // Write to file in logs directory within appDir
+    const logDir = path.join(appDir, 'logs');
+    const logFile = path.join(logDir, `${year}-${month}-${day}.log`);
+
+    // Create logs directory if it doesn't exist
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir, { recursive: true });
+    }
+
+    fs.appendFile(logFile, fullMessage, (err) => {
+        if (err) {
+            console.error("❌ Failed to write log file:", err);
+        }
+    });
+}
+
+function ipUpdaterForDotNetSDK(mainWindow, jsonPath) {
+    const jsonData = fs.readFileSync(jsonPath, 'utf-8'); // read as string
+    const data = JSON.parse(jsonData);
+
+    data.urls = `http://${ipv4Address}:8080`;
+    data.Options.LocalIP = ipv4Address;
+
+    const updatedJsonData = JSON.stringify(data, null, 2);
+
+    // Write the updated JSON data to the file
+    fs.writeFile(jsonPath, updatedJsonData, (err) => {
+        if (err) throw err;
+        log(mainWindow, `[Device] JSON file has been updated! `);
+    });
+}
+
+function stopProcess(mainWindow, Process) {
+
+    if (!Process) {
+        log(mainWindow, `Something went wrong ${Process}.`);
+        return;
+    }
+
+    Process.kill();
+    Process = null;
+    log(mainWindow, `${Process} has been stopped.`);
+
+}
+
+function cloneTheRepoIfRequired(mainWindow, appDir, targetDir, backendDir, phpPath, repoUrl) {
+    const composerPhar = path.join(appDir, 'composer.phar');
+
+    if (!fs.existsSync(targetDir) || fs.readdirSync(targetDir).length === 0) {
+        log(mainWindow, `Cloning repository from ${repoUrl}`);
+        const git = simpleGit();
+
+        git.clone(repoUrl, targetDir)
+            .then(() => {
+                log(mainWindow, 'Repository cloned successfully.');
+
+                // Run `composer install`
+                return spawnWrapper(mainWindow, "Repo", phpPath, [composerPhar, 'install'], {
+                    cwd: backendDir
+                });
+            })
+            .then(() => {
+                // Run `php artisan migrate --force`
+                log(mainWindow, 'Running php artisan migrate --force');
+                return spawnWrapper(mainWindow, "Repo", phpPath, ['artisan', 'migrate', '--force'], {
+                    cwd: backendDir
+                });
+            })
+            .then(() => {
+                log(mainWindow, 'Migration completed successfully.');
+            })
+            .catch(err => {
+                log(mainWindow, `Error: ${err.message}`);
+            });
+    } else {
+        // log(mainWindow, 'Repository already cloned.');
+    }
+}
+
+const timezoneOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false, // Use 24-hour format
+    timeZone: "Asia/Dubai",
+};
+
+const verification_methods = {
+    1: "Card",
+    2: "Fing",
+    3: "Face",
+    4: "Fing + Card",
+    5: "Face + Fing",
+    6: "Face + Card",
+    7: "Card + Pin",
+    8: "Face + Pin",
+    9: "Fing + Pin",
+    10: "Manual",
+    11: "Fing + Card + Pin",
+    12: "Face + Card + Pin",
+    13: "Face + Fing + Pin",
+    14: "Face + Fing + Card",
+    15: "Repeated",
+};
+
+const reasons = {
+    16: "Date Expire",
+    17: "Timezone Expire",
+    18: "Holiday",
+    19: "Unregistered",
+    20: "Detection lock",
+    23: "Loss Card",
+    24: "Blacklisted",
+    25: "Without Verification",
+    26: "No Card Verification",
+    27: "No Fingerprint",
+};
+
+function getFormattedDate() {
+    const [newDate, newTime] = new Intl.DateTimeFormat("en-US", timezoneOptions)
+        .format(new Date())
+        .split(",");
+    const [m, d, y] = newDate.split("/");
+
+    return {
+        date: `${d.padStart(2, 0)}-${m.padStart(2, 0)}-${y}`,
+        time: newTime,
+    };
+}
+
+function notify(title = "", body = "", icon = 'favicon-256x256.png', onClick = null) {
+    const notification = new Notification({
+        title,
+        body,
+        icon: path.join(appDir, 'www', icon)
+    });
+
+    if (onClick && typeof onClick === 'function') {
+        notification.on('click', onClick);
+    }
+    notification.show();
+}
+
+module.exports = {
+    log,
+    tailLogFile,
+    spawnWrapper,
+    stopProcess,
+    cloneTheRepoIfRequired,
+    getFormattedDate, ipUpdaterForDotNetSDK, notify,
+    timezoneOptions, verification_methods, reasons, ipv4Address
+}
---
+++ main.js
@@ -0,0 +1,217 @@
+const { app, BrowserWindow, ipcMain, screen, } = require('electron');
+
+const path = require('path');
+const { spawn } = require('child_process');
+const fs = require("fs")
+const WebSocket = require("ws");
+
+app.setName('GymPro');
+app.setAppUserModelId('GymPro');
+
+const { log, spawnWrapper, stopProcess, getFormattedDate, ipUpdaterForDotNetSDK, notify, verification_methods, reasons, ipv4Address } = require('./helpers');
+const { initAutoUpdater } = require('./updater');
+
+const isDev = !app.isPackaged;
+
+let appDir;
+if (isDev) {
+  appDir = path.join(__dirname);
+} else {
+  appDir = process.resourcesPath; // where extraResources are placed
+}
+
+const srcDirectory = path.join(appDir, 'backend');
+const phpPath = path.join(srcDirectory, 'php');
+const dotnetSDK = path.join(appDir, 'dotnet_sdk');
+const javaSDK = path.join(appDir, 'java_sdk');
+const jarPath = path.join(javaSDK, 'SxDeviceManager.jar');
+const jsonPath = path.join(dotnetSDK, 'appsettings.json');
+
+const nginxPath = path.join(appDir, 'nginx.exe');
+const phpPathCli = path.join(phpPath, 'php.exe');
+const phpCGi = path.join(phpPath, 'php-cgi.exe');
+
+
+const dotnetExe = path.join(dotnetSDK, 'dotnet', 'dotnet.exe');
+const javaExe = path.join(javaSDK, 'bin', 'java.exe');
+
+let mainWindow;
+let nginxWindow;
+
+let NginxProcess;
+let ScheduleProcess;
+let QueueProcess;
+let dotnetSDKProcess;
+let javaSDKProcess;
+
+
+function startWebSocketClient(mainWindow) {
+
+  const SOCKET_ENDPOINT = `ws://${ipv4Address}:8080/WebSocket`;
+  const logFilePathAlarm = `./backend/storage/app/alarm/alarm-logs-${getFormattedDate().date}.csv`;
+
+  function connect() {
+
+    log(mainWindow, `[LISTENER] Attempting to connect to ${SOCKET_ENDPOINT}...`);
+
+    const socket = new WebSocket(SOCKET_ENDPOINT);
+
+    socket.onopen = () => {
+      log(mainWindow, `[LISTENER] Connected to ${SOCKET_ENDPOINT}`);
+    };
+
+    socket.onerror = (error) => {
+      log(mainWindow, "[LISTENER] WebSocket error:", error.message || error);
+      // Retry connection after 3 seconds
+      setTimeout(connect, 3000);
+    };
+
+    socket.onclose = (event) => {
+      log(mainWindow, `[LISTENER] WebSocket connection closed with code ${event.code} at ${getFormattedDate().date} ${getFormattedDate().time}`);
+      // Retry connection after 3 seconds
+      // setTimeout(connect, 3000);
+    };
+
+    socket.onmessage = ({ data }) => {
+      let logFilePath = `./backend/storage/app/logs-${getFormattedDate().date}.csv`;
+      try {
+        const jsonData = JSON.parse(data).Data;
+        const { UserCode, SN, RecordDate, RecordNumber, RecordCode } = jsonData;
+
+        if (UserCode > 0) {
+          let status = RecordCode > 15 ? "Access Denied" : "Allowed";
+          let mode = verification_methods[RecordCode] ?? "---";
+          let reason = reasons[RecordCode] ?? "---";
+          const logEntry = `${UserCode},${SN},${RecordDate},${RecordNumber},${status},${mode},${reason}`;
+          fs.appendFileSync(logFilePath, logEntry + "\n");
+          log(mainWindow, `[LISTENER] ${logEntry}`);
+        }
+
+        if (UserCode == 0 && RecordCode == 19) {
+          const alarm_logEntry = `${SN},${RecordDate}`;
+          fs.appendFileSync(logFilePathAlarm, alarm_logEntry + "\n");
+          log(mainWindow, "[LISTENER] Error processing message: " + alarm_logEntry);
+        }
+      } catch (error) {
+        log(mainWindow, "[LISTENER] Error processing message: " + error.message);
+      }
+    };
+  }
+
+  connect();
+}
+
+function createWindow() {
+
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  mainWindow = new BrowserWindow({
+    width,
+    height,
+    show: false, // enable to hide the window
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.webContents.once('did-finish-load', () => {
+    ipUpdaterForDotNetSDK(mainWindow, jsonPath);
+    startServices(mainWindow);
+    // initAutoUpdater(mainWindow);
+  });
+}
+
+function startServices(mainWindow) {
+  const address = `http://${ipv4Address}:3001`;
+
+  spawnWrapper(mainWindow, "[Application]", phpCGi, ['-b', `127.0.0.1:9000`], {
+    cwd: appDir
+  });
+
+  NginxProcess = spawnWrapper(mainWindow, "[Nginx]", nginxPath, { cwd: appDir });
+
+  log(mainWindow, `[Application] started on ${address}`);
+
+  dotnetSDKProcess = spawnWrapper(mainWindow, "[Device]", dotnetExe, ['FCardProtocolAPI.dll'], {
+    cwd: dotnetSDK
+  });
+
+  dotnetSDKProcess.stdout.on('data', (data) => {
+    if (data.includes('Now listening on')) {
+      startWebSocketClient(mainWindow);
+    }
+  });
+
+  ScheduleProcess = spawnWrapper(mainWindow, "[Application]", phpPathCli, ['artisan', 'schedule:work'], {
+    cwd: srcDirectory
+  });
+
+  QueueProcess = spawnWrapper(mainWindow, "[Application]", phpPathCli, ['artisan', 'queue:work'], {
+    cwd: srcDirectory
+  });
+
+  javaSDKProcess = spawnWrapper(mainWindow, "[Device]", javaExe, ['-jar', jarPath], {
+    cwd: javaSDK
+  });
+
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  // ✅ Only create a new window if it's not already open
+  if (!nginxWindow || nginxWindow.isDestroyed()) {
+    nginxWindow = new BrowserWindow({
+      width,
+      height,
+      frame: true,
+      fullscreen: false,
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true
+      }
+    });
+
+    nginxWindow.loadURL(`http://${ipv4Address}:3001`);
+    nginxWindow.maximize();
+
+    nginxWindow.on('closed', () => {
+      nginxWindow = null;
+    });
+  } else {
+    nginxWindow.focus(); // ✅ Bring existing window to front
+  }
+}
+
+function stopServices(mainWindow) {
+  stopProcess(mainWindow, ScheduleProcess);
+  stopProcess(mainWindow, QueueProcess);
+  stopProcess(mainWindow, NginxProcess);
+  stopProcess(mainWindow, dotnetSDKProcess);
+  stopProcess(mainWindow, javaSDKProcess);
+
+  const forFullStop = spawn('taskkill', ['/F', '/IM', 'nginx.exe']);
+  forFullStop.on('close', () => {
+    log(mainWindow, '[Application] Server stopped.');
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  ipcMain.on('start-server', () => startServices(mainWindow));
+  ipcMain.on('stop-server', () => stopServices(mainWindow));
+});
+let isQuitting = false;
+
+app.on('before-quit', (e) => {
+  if (!isQuitting) {
+    e.preventDefault(); // prevent quit
+    log(mainWindow, "Stopping services before quitting...");
+    stopServices(mainWindow); // assume this is sync or finishes quickly
+    isQuitting = true;
+    app.quit(); // trigger quit again
+  }
+});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
